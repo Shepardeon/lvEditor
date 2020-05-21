7dd71a89334ce80b96f555f7e8814bd2ed2a807a
Thomas De Maen <mr-sylar59@world-of-sylar.fr>
2020-05-21 21:13:12 +0200

    Empty project

---
+++ res/templates/emptyProject/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(p_conf)
+    p_conf.window.title = "New LvGame"
+    p_conf.window.width = 1024
+    p_conf.window.height = 576
+end
---
+++ res/templates/emptyProject/main.lua
@@ -0,0 +1,11 @@
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.print("Hello World!", love.graphics.getWidth()/2, love.graphics.getHeight()/2)
+end
